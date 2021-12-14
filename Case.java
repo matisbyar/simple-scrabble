92b9db24c0a28436000c2180ae57c0152f32a587
matisvigne <86782053+matisvigne@users.noreply.github.com>
2021-12-14 09:55:25 +0100

    Création de la classe Case
    Création de la classe Case et de ses méthodes :
        - public Case(int uneCouleur) : constructeur
        - public int getCouleur() : renvoie la couleur de this
        - public char getLettre() : renvoie la lettre de this
        - public void setLettre(char let) : écrase l'ancienne lettre de this.lettre en la nouvelle lettre "let"
        - public boolean estRecouverte : vérifie si la lettre est recouverte -> stratégie : renvoie true ssi la lettre ne contient pas une chaîne de caractère vide
        - public String toString() : renvoie la description de la case
    
    ———
    Je suis confiant dans les méthodes effectuées, une relecture n'est toutefois pas inutile.

---
+++ Case.java
@@ -0,0 +1,35 @@
+public class Case {
+    private int couleur;            // la couleur est un entier entre 1 et 5
+    private char lettre = ' ';       // si la lettre est recouverte, la variable lettre contient la contient
+    
+    public Case(int uneCouleur){
+        /* pré-requis : uneCouleur est un entier entre 1 et 5
+           action : constructeur de Case */
+        this.couleur = uneCouleur;
+    }
+
+    public int getCouleur(){
+        /* résultat : la couleur de this, un nombre entre 1 et 5 */
+        return this.couleur;
+    }
+
+    public char getLettre(){
+        /* pré-requis : cette case est recouverte */
+        // JE PENSE QUE CELA EST FAUX
+        return this.lettre;
+    }
+
+    public void setLettre(char let){
+        /* pré-requis : let est une lettre majuscule */
+        this.lettre = let;
+    }
+
+    public boolean estRecouverte(){
+        /* résultat : vrai ssi la case est recouverte par une lettre */
+        return lettre != ' ';
+    }
+
+    public String toString(){
+        return "La case possède une couleur de valorisation " + this.couleur + " et elle possède cette lettre (aucune si vide) : " + this.lettre + "\"";
+    }
+}
