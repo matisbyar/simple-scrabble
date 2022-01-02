2c1ca2997c9ede7af298bbd694d9114f208d8d2e
matisvigne <86782053+matisvigne@users.noreply.github.com>
2022-01-02 16:35:07 +0100

    Update Plateau.java
        - Correction de placementLibre sur les erreurs que l'IDE affichait.
        - Ajout de la fonction nbPointsPlacement

--- Plateau.java
+++ Plateau.java
@@ -1,5 +1,5 @@
 public class Plateau {
-    private Case[][] g;     // g pour grille
+    private Case[][] g = new Case[15][15];     // g pour grille
 
     public Plateau(){
         int[][] plateau = { {5, 1, 1, 2, 1, 1, 1, 5, 1, 1, 1, 2, 1, 1, 5},
@@ -17,6 +17,11 @@ public class Plateau {
                             {1, 1, 4, 1, 1, 1, 2, 1, 2, 1, 1, 1, 4, 1, 1},
                             {1, 4, 1, 1, 1, 3, 1, 1, 1, 3, 1, 1, 1, 4, 1},
                             {5, 1, 1, 2, 1, 1, 1, 5, 1, 1, 1, 2, 1, 1, 5}};
+        for(int i = 0; i < plateau.length; i++){
+            for(int j = 0; j < plateau.length; j++){
+                this.g[i][j] = new Case(plateau[i][j]);
+            }
+        }
     }
 
     public Plateau(Case[][] plateau){
@@ -39,47 +44,121 @@ public class Plateau {
     }
 
     public boolean placementValide(String mot, int numLig, int numCol, char sens, MEE e){
-        /* pré-requis : mot est un mot accepté par CapeloDico,
-                       0 <= numLig <= 14, 0 <= numCol <= 14, sens est un élément de {'h', 'v'}
+        /* pré-requis : mot est un mot accepté par CapeloDico, 0 <= numLig <= 14, 
+                       0 <= numCol <= 14, sens est un élément de {'h', 'v'}
                        et l'entier maximum prévu pour e est au moins 25 
            résultat : retourne vrai ssi le placement de mot sur this à partir de la case (numLig, numCol) dans le sens donné par sens
                       à l'aide des jetons de e est valide. */
-        // VOIR LE SUJET POUR TOUTES LES VÉRIFICATIONS
         boolean verification = true;
-        int finVerification = 0;
         int moitie = (g.length+1)/2;
         
         // Dans le cas où le plateau est vide
         if (plateau[moitie][moitie] == 4) {
-            while (verification || finVerification == 0) {
+            while (verification) {
                 // Le mot proposé a au moins 2 lettres
-                if (mot.length < 2) {
+                if (mot.length() < 2) {
                     verification = false;
                 }
                 // La zone de placement du mot contient la case centrale du plateau
                 else if (numLig != moitie && numCol != moitie) {
                     verification = false;
                 }
-                else if (numLig == moitie && (sens = 'v' || numCol > moitie || numCol+mot.length < moitie)) {
+                else if (numLig == moitie && (sens == 'v' || numCol > moitie || numCol + mot.length() < moitie)) {
                     verification = false;
                 }
-                else if (numCol == moitie && (sens = 'h' || numLig > moitie || numLig+mot.length < moitie)) {
+                else if (numCol == moitie && (sens == 'h' || numLig > moitie || numLig + mot.length() < moitie)) {
                     verification = false;
                 }
                 // Le chevalet du joueur proposant le mot contient les lettres permettant de former le mot
-                MEE chevalet = MEE(e);
-                for (int i = 0; i < mot.length; i++) {
+                MEE chevalet = new MEE(e);
+                for (int i = 0; i < mot.length(); i++) {
                     verification = chevalet.retire(Ut.majToIndex(mot.charAt(i)));
                 }
-                finVerification = 1;
+
             }
         }
         // Dans le cas où le plateau n'est pas vide
         else {
-            while (verification || finVerification == 0) {
+            while (verification) {
                 // La zone de placement du mot ne dépasse pas de la grille
-                if ((sens == 'h' && numCol+mot.length >= g.length) || (sens == 'v' && numLig+mot.length >= g.length)) {
+                if ((sens == 'h' && numCol + mot.length() >= g.length) || (sens == 'v' && numLig + mot.length() >= g.length)) {
                     verification = false; 
                 }
+            }
+        }
+        return verification;
+    }
+
+    public int nbPointsPlacement(String mot, int numLig, int numCol, char sens, int[] nbPointsJet){
+        /* pré-requis : le placement de mot sur this à partir de la case
+                        n(numLig, numCol) dans le sens donné par sens est valide
+            résultat : retourne le nombre de points rapportés par ce placement, le nombre de points de chaque jeton étant donné par le tableau nbPointsJet. */
+        int nbPointsPlacement = 0;
+        for(int i = 0; i < mot.length(); i++){
+            // Dans le cas où le sens est horizontal et que la case est grise (pas de valorisation)
+            if(sens == 'h' && plateau[numLig][numCol] == '1'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))];
+                numLig++;
+            }
+            // Dans le cas où le sens est horizontal et que la case est bleu clair (lettre compte double)
+            else if(sens == 'h' && plateau[numLig][numCol] == '2'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))] * 2;
+                numLig++;
+            }
+            // Dans le cas où le sens est horizontal et que la case est bleue (lettre compte triple)
+            else if(sens == 'h' && plateau[numLig][numCol] == '3'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))] * 3;
+                numLig++;
+            }
+            // Dans le cas où le sens est horizontal et que la case est rose (mot compte double)
+            else if(sens == 'h' && plateau[numLig][numCol] == '4'){
+                for(int j = 0; i < mot.length(); j++){
+                    nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))];
+                    numLig++;
+                }
+                nbPointsPlacement *= 2;
+            }
+            // Dans le cas où le sens est horizontal et que la case est rouge (mot compte triple)
+            else if(sens == 'h' && plateau[numLig][numCol] == '5'){
+                for(int j = 0; i < mot.length(); j++){
+                    nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))];
+                    numLig++;
+                }
+                nbPointsPlacement *= 3;
+            }
+
+            // Dans le cas où le sens est vertical et que la case est grise (pas de valorisation)
+            if(sens == 'v' && plateau[numLig][numCol] == '1'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))];
+                numCol++;
+            }
+            // Dans le cas où le sens est vertical et que la case est bleu clair (lettre compte double)
+            else if(sens == 'v' && plateau[numLig][numCol] == '2'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))] * 2;
+                numCol++;
+            }
+            // Dans le cas où le sens est vertical et que la case est bleue (lettre compte triple)
+            else if(sens == 'v' && plateau[numLig][numCol] == '3'){
+                nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(i))] * 3;
+                numCol++;
+            }
+            // Dans le cas où le sens est vertical et que la case est rose (mot compte double)
+            else if(sens == 'v' && plateau[numLig][numCol] == '4'){
+                for(int j = 0; j < mot.length(); j++){
+                    nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(j))];
+                    numCol++;
+                }
+                nbPointsPlacement *= 2;
+            }
+            // Dans le cas où le sens est vertical et que la case est rouge (mot compte triple)
+            else if(sens == 'v' && plateau[numLig][numCol] == '5'){
+                for(int j = 0; j < mot.length(); j++){
+                    nbPointsPlacement += nbPointsJet[Ut.majToIndex(mot.charAt(j))];
+                    numCol++;
+                }
+                nbPointsPlacement *= 3;
+            }
+        }
+        return nbPointsPlacement;
     }
 }
